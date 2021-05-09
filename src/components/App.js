5cacaa1002c431270d2cb59b07c610999d06c6dd
gebrielmbelaineh <gebriel.belaineh.ug@dartmouth.edu>
2021-05-08 20:55:15 -0400

    axios tutorial

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,47 @@
-import React from 'react';
-import {
-  BrowserRouter as Router, Route, NavLink, Switch,
-} from 'react-router-dom';
-import Controls from './controls';
-import Counter from './counter';
-
-const About = (props) => {
-  return <div> All there is to know about me</div>;
-};
-
-const Welcome = (props) => {
-  return <div>Welcome</div>;
-};
-
-const Nav = (props) => {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-      </ul>
-    </nav>
-  );
-};
-
-const Test = (props) => {
-  return <div> ID: {props.match.params.id}</div>;
-};
-
-const FallBack = (props) => {
-  return <div> URL Not found</div>;
-};
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = (props) => {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/about" component={About} />
-          <Route exact path="/test/:id" component={Test} />
-          <Route component={FallBack} />
-        </Switch>
-        <Counter />
-        <Controls />
+  const [weather, setWeather] = useState(null);
+  const [input, setInput] = useState('');
+  useEffect(() => {
+    axios.get('http://api.weatherapi.com/v1/current.json?key=826fb0c3b8b347919d974714210805&q=Addis Ababa&aqi=no')
+      .then((data) => {
+        setWeather(data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
+  const weatherInput = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleClick = () => {
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=826fb0c3b8b347919d974714210805&q=${input}`)
+      .then((data) => {
+        setWeather(data.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      {weather && (
+      <div>
+        <div className="search">
+          <input type="text" onChange={weatherInput} />
+          <button type="button" onClick={handleClick}>Submit</button>
+        </div>
+        <h1> {weather.location.country }</h1>
+        <h2> {weather.location.region}</h2>
+        <div>
+          <h2>{weather.current.condition.text}</h2>
+          <img src={weather.current.condition.icon} alt="" />
+          <h3>{weather.current.temp_c}</h3>
+        </div>
       </div>
-    </Router>
+      )}
+    </div>
   );
 };
 
